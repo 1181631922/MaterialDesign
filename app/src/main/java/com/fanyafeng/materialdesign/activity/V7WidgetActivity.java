0620fdd188e0b5bfd79f27ebc25d859f40594c47
fanyafeng <fanyafeng@365rili.com>
2016-06-14 17:17:14 +0800

    widger

---
+++ app/src/main/java/com/fanyafeng/materialdesign/activity/V7WidgetActivity.java
@@ -0,0 +1,42 @@
+package com.fanyafeng.materialdesign.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.fanyafeng.materialdesign.BaseActivity;
+import com.fanyafeng.materialdesign.R;
+
+public class V7WidgetActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_v7_widget);
+
+        title = "v7包中的一些控件";
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        initView();
+        initData();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+
+}
